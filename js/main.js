4518565baa751333c68a4104a4f11084550ffd24
zemk <shapoval.44105@gmail.com>
2021-09-22 15:18:32 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -31,3 +31,19 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+var modal = document.getElementById('myModal');
+ 
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+ btn.onclick = function() {
+  modal.style.display = "block";
+}
+ span.onclick = function() {
+  modal.style.display = "none";
+}
+ window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
